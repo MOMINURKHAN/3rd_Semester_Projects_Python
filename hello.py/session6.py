b44523bebda2ee74a1f77f0e68b731c9d489b36f
MD MOMINUR KHAN <khanmominul527@gmail.com>
2025-12-12 06:51:41 +0800

    session 6 - Dictionary,input,print,func - these things

--- hello.py/session6.py
+++ hello.py/session6.py
@@ -1,41 +1,60 @@
-numbers = [2,-34,6,0]
+#task 1 
+def make_pizza():
+    print(f"making your pizza - (task-1)")
 
-for n in numbers:
-    if n>0:
-        print(n,"is positive")
-    else:
-        print(n,"is negative")
+make_pizza()
+#task 2 + task3
+def make_pizza_size(size,topping):
+    print(f"Making a {size} inch pizza with {topping}")
+
+#make_pizza_size(12,'honey')
+
+#task 4
+#make_pizza_size(int(input("Enter the pizza size")),input("Enter the topping you want "))
+user_order = 1
+all_order = []
+while user_order<3:
+    #task 5
+    size = int(input("Enter size : "))
+    topping = input("Enter your toppings ").split(',')
+    make_pizza_size(size,topping=topping)
+
+    def task_5(size,topping):
+        x = []
+        x = " ".join(topping)
+        dictionary = {
+            'size' : size,
+            'topping' : topping,
+        }
+        return dictionary
+        
+
+    #task 8 
+
+    order = (task_5(15,topping=topping))
+    # print(order)
+
+    #task 9
+    order['crust'] = input("Enter crust : ")
+    # print(order)
+
+    #task 10
+    user_choice = input("Delivery?(Yes/No)").lower()
+    order['delivery'] = True if user_choice=='yes' else False
+    user_order+=1
+    all_order.append(order)
+    
 
 
- 
+#task 11
+#print(f"{order['size']} inch pizza with {order['topping']} 'crust' : {order['crust']} 'Delivery' : {order['delivery']}")
 
-def FirstFunc():
-    birth_year = int(input("Enter your birth year : "))
-    if birth_year > (2008):
-        print("You are still a kid go back to home")
-    elif birth_year<2000:
-        print("You're also not eligible why you came here go to the elder's classroom")
-    else:
-        print("You can continue your study with us")
+#Task 12
+for i in all_order:
+    print(f"{i['size']} inch pizza with {i['topping']} 'crust' : {i['crust']} 'Delivery' : {i['delivery']}")
 
-FirstFunc()
-
-def second_func(name,age,Language = "English"):
-    print("Hello How are you",name ,"What's your age ",age)
-    print("He's learning",Language,"Language")
-
-second_func("Mominur",23,Language="Chinese")
+#while doing this H.W I didn't notice i need to change the same function in every task so i made similar function several times, that's my mistake but i only apply the while loop 
+#in the last func  and other components for two times and it's working properly
 
 
-print("Hello Khan !", end=" sss ", sep= " sldfj ")
-
-def returningFunc():
-    c = int(input("Enter a number : "))
-    d = c*107
-    return d
-
-print("wanna get 107 times of any number then proceed with the instructuction")
-
-f = returningFunc()
-print("this is the 107 times of your provided number is :",f)
 
